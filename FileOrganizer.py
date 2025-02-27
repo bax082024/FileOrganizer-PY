fadf242857303a7040cacbee54d62d8d78a2ce7f
bax082024 <bax082024@gmail.com>
2025-02-27 11:50:50 +0100

    added print found files function

--- FileOrganizer.py
+++ FileOrganizer.py
@@ -8,4 +8,9 @@ if not os.path.exists(folder_path):
 
 files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
 
+print("\n Found Files:")
+for file in files:
+    print(f"- {file}")
 
+
+print("\n Step 1 Complete: Folder scanned successfully!")
