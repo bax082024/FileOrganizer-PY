7e5715fdb0c7b3a910a83f6a12734c3f8122d790
bax082024 <bax082024@gmail.com>
2025-02-27 11:49:12 +0100

    added get list of all files in folder

--- FileOrganizer.py
+++ FileOrganizer.py
@@ -5,3 +5,7 @@ folder_path = input("Enter the folder path to organize: ").strip()
 if not os.path.exists(folder_path):
     print("Error: The specified folder does not exist. Please check the path and try again.")
     exit()
+
+files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+
+
